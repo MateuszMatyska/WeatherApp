22f5da10781bcb63a35befd95b72ec9f0a060519
mateuszmatyska <mateuszmatyska@gmail.com>
2020-04-17 22:41:25 +0200

    navigation ref

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Drawer from 'src/navigation/drawerNavigation/Drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import {navigationRef} from 'src/navigation/NavigationRef';
 
 const App = () => {
   return (
-   <NavigationContainer>
+   <NavigationContainer ref={navigationRef}>
      <Drawer />
    </NavigationContainer>
   );

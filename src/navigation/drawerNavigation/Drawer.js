2f634116eef440f9b3c20669c1efd3580ca4a427
mateuszmatyska <mateuszmatyska@gmail.com>
2020-04-20 10:40:35 +0200

    drawer content

--- src/navigation/drawerNavigation/Drawer.js
+++ src/navigation/drawerNavigation/Drawer.js
@@ -4,14 +4,27 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import About from "src/screens/about/About";
 import Tabs from 'src/navigation/tabNavigation/Tabs';
 import {colors} from 'src/const/colors'
+import DrawerContent from 'src/navigation/drawerNavigation/drawerContent/DrawerContent';
 
 const Drawer = () => {
   const Drawer = createDrawerNavigator();
 
   return (
-    <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Tabs} />
-        <Drawer.Screen name="About" component={About} />
+    <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen 
+          name="Home"
+          component={Tabs} 
+          options={{
+            drawerIcon: ({color}) => (<Icon name="home" color={color} type="FontAwesome" style={{color: `${color}`}}/>)
+          }}
+        />
+        <Drawer.Screen 
+          name="About" 
+          component={About} 
+          options={{
+            drawerIcon: ({color}) => (<Icon name="info" color={color} type="FontAwesome" style={{color: `${color}`}}/>)
+          }}
+        />
       </Drawer.Navigator>
   );
 };
---
+++ src/navigation/drawerNavigation/drawerContent/DrawerContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    DrawerContentScrollView,
+    DrawerItemList,
+} from '@react-navigation/drawer';
+
+const DrawerContent = (props) => {
+    return <DrawerContentScrollView {...props}>
+
+      <DrawerItemList {...props} />
+  </DrawerContentScrollView>
+}
+
+export default DrawerContent;
